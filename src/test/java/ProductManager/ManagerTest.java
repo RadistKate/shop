2f5be504027ff3e7d68c19a33012889af21fbf27
Katy test <artebyakinae@mail.ru>
2023-03-11 17:41:08 +0300

    New commit

---
+++ src/test/java/ProductManager/ManagerTest.java
@@ -0,0 +1,71 @@
+package ProductManager;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ManagerTest {
+    @Test
+    public void testSearchByName() {
+        Repository repo = new Repository();
+        Manager manager = new Manager(repo);
+        Book book1 = new Book(1, "Shantaram", 800, "Roberts");
+        Book book2 = new Book(2, "Shantaram2", 800, "Roberts");
+        Book book3 = new Book(3, "Idiot", 600, "Dostoevsky");
+
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+
+        Product[] actual = manager.searchBy("Shantaram");
+        Product[] expected = {book1, book2};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testAdd() {
+        Repository repo = new Repository();
+        Manager manager = new Manager(repo);
+        Book book1 = new Book(1, "Shantaram", 800, "Roberts");
+        Book book2 = new Book(2, "Shantaram2", 800, "Roberts");
+        Book book3 = new Book(3, "Idiot", 600, "Dostoevsky");
+
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+
+        Product[] actual = repo.findAll();
+        Product[] expected = {book1, book2, book3};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testAddAll() {
+        Repository repo = new Repository();
+        Manager manager = new Manager(repo);
+        Book book1 = new Book(1, "Shantaram", 800, "Roberts");
+        Book book2 = new Book(2, "Shantaram2", 800, "Roberts");
+        Book book3 = new Book(3, "Idiot", 600, "Dostoevsky");
+        Smartphone smartphone1 = new Smartphone(4, "Nokia", 5000, "Japan");
+        Smartphone smartphone2 = new Smartphone(4, "Samsung", 5000, "Japan");
+
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+        manager.add(smartphone1);
+        manager.add(smartphone2);
+
+        Product[] actual = repo.findAll();
+        Product[] expected = {book1, book2, book3, smartphone1, smartphone2};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+
+}
---
+++ src/test/java/ProductManager/RepositoryTest.java
@@ -0,0 +1,49 @@
+package ProductManager;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RepositoryTest {
+    @Test
+    public void testAdd() {
+        Repository repo = new Repository();
+
+        Book book1 = new Book(1, "Shantaram", 800, "Roberts");
+        Book book2 = new Book(2, "Shantaram2", 800, "Roberts");
+        Book book3 = new Book(3, "Idiot", 600, "Dostoevsky");
+
+        repo.add(book1);
+        repo.add(book2);
+        repo.add(book3);
+
+        Product[] actual = repo.findAll();
+        Product[] expected = {book1, book2, book3};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testRemoveById() {
+        Repository repo = new Repository();
+
+        Book book1 = new Book(1, "Shantaram", 800, "Roberts");
+        Book book2 = new Book(2, "Shantaram2", 800, "Roberts");
+        Book book3 = new Book(3, "Idiot", 600, "Dostoevsky");
+
+        repo.add(book1);
+        repo.add(book2);
+        repo.add(book3);
+        repo.removeById(1);
+
+        Product[] actual = repo.findAll();
+        Product[] expected = {book2, book3};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+
+}
