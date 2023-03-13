0bdc0ff1f30a83d408d76adbcd4839a2a9fa0bc7
Katy test <artebyakinae@mail.ru>
2023-03-13 13:30:22 +0300

    Rework commit

--- src/test/java/ProductManager/ManagerTest.java
+++ src/test/java/ProductManager/ManagerTest.java
@@ -6,24 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ManagerTest {
-    @Test
-    public void testSearchByName() {
-        Repository repo = new Repository();
-        Manager manager = new Manager(repo);
-        Book book1 = new Book(1, "Shantaram", 800, "Roberts");
-        Book book2 = new Book(2, "Shantaram2", 800, "Roberts");
-        Book book3 = new Book(3, "Idiot", 600, "Dostoevsky");
 
-        manager.add(book1);
-        manager.add(book2);
-        manager.add(book3);
-
-        Product[] actual = manager.searchBy("Shantaram");
-        Product[] expected = {book1, book2};
-
-        Assertions.assertArrayEquals(expected, actual);
-
-    }
 
     @Test
     public void testAdd() {
@@ -66,6 +49,61 @@ class ManagerTest {
         Assertions.assertArrayEquals(expected, actual);
 
     }
+    @Test
+    public void testSearchByName() {
+        Repository repo = new Repository();
+        Manager manager = new Manager(repo);
+        Book book1 = new Book(1, "Shantaram", 800, "Roberts");
+        Book book2 = new Book(2, "Shantaram2", 800, "Roberts");
+        Book book3 = new Book(3, "Idiot", 600, "Dostoevsky");
+
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+
+        Product[] actual = manager.searchBy("Shantaram");
+        Product[] expected = {book1, book2};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+    @Test
+    public void testSearchByNameSecond() {
+        Repository repo = new Repository();
+        Manager manager = new Manager(repo);
+        Book book1 = new Book(1, "Shantaram", 800, "Roberts");
+        Book book2 = new Book(2, "Shantaram2", 800, "Roberts");
+        Book book3 = new Book(3, "Idiot", 600, "Dostoevsky");
+
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+
+        Product[] actual = manager.searchBy("Shantaram2");
+        Product[] expected = { book2};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+    @Test
+    public void testSearchByNameThird() {
+        Repository repo = new Repository();
+        Manager manager = new Manager(repo);
+        Book book1 = new Book(1, "Shantaram", 800, "Roberts");
+        Book book2 = new Book(2, "Shantaram2", 800, "Roberts");
+        Book book3 = new Book(3, "Idiot", 600, "Dostoevsky");
+
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+
+        Product[] actual = manager.searchBy("War and peace");
+        Product[] expected = {};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
 
 
 }
